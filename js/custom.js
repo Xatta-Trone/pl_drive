a226075a57ab2770db72a93ac7f37befc83d2e5e
Monzurul Islam <monzurul.ce.buet@gmail.com>
2017-10-20 20:45:47 +0600

    1st commit done

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	//scroll to top
+	 $(window).scroll(function() {
+	    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+	        $('#return-to-top').fadeIn(200);   // Fade in the arrow
+	        $("#sticker").addClass("lightheader");
+	    } else {
+	        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+	        $("#sticker").removeClass("lightheader");
+	    }
+	});
+	$('#return-to-top').click(function() {      // When arrow is clicked
+	    $('body,html').animate({
+	        scrollTop : 0                       // Scroll to top of body
+	    }, 500);
+	});
+
+	/*$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 50) {
+        //clearHeader, not clearheader - caps H
+        $("#sticker").addClass("lightheader");
+    }
+	}); */
+	   
+  });
